90e680500b4db63113b185ddab29db939cbcc067
Will <wor101@gmail.com>
2021-12-05 16:11:29 -0500

    added middleware to hello_world in part3

--- part3/hello_world/index.js
+++ part3/hello_world/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:', request.path);
+  console.log('Body:', request.body);
+  console.log('---');
+  next();
+}
 
 app.use(express.json())
+app.use(requestLogger)
 
 let notes = [
   {
@@ -75,6 +83,11 @@ app.post('/api/notes', (request, response) => {
   
   response.json(note)
 })
+
+const unknownEndPoint = (request, response) => {
+  response.status(404).send({error: 'unkown endpoint'})
+}
+app.use(unknownEndPoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
