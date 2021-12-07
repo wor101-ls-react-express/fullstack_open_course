9d9af2d374cc445bf02a397af552cfb60c2fcca7
Will <wor101@gmail.com>
2021-12-07 14:16:27 -0500

    updated frontend of example note app

--- part2/part2_example/src/App.js
+++ part2/part2_example/src/App.js
@@ -70,7 +70,7 @@ function App() {
   }
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
+    //const url = `http://localhost:3001/api/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important}
 
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,22 +1,22 @@
 import axios from 'axios'
 
 const getAll = () => {
-  const request = axios.get('http://localhost:3001/persons')
+  const request = axios.get('/api/persons')
   return request.then(response => response.data)
 }
 
 const addPerson = (person) => {
-  const request = axios.post('http://localhost:3001/persons', person)
+  const request = axios.post('/api/persons', person)
   return request.then(response => response.data)
 }
 
 const deletePerson = (person) => {
-  const request = axios.delete(`http://localhost:3001/persons/${person.id}`)
+  const request = axios.delete(`/api/persons/${person.id}`)
   return request.then(response => response.data)
 }
 
 const updateNumber = (person) => {
-  const request = axios.put(`http://localhost:3001/persons/${person.id}`, person)
+  const request = axios.put(`/api/persons/${person.id}`, person)
   return request.then(response => response.data)
 }
 
